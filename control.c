fdf715775cb0a4a29d403b79759348c64e4d0c22
Patrick Langan <patrick.g.langan@gmail.com>
2015-09-13 23:40:14 +0000

    Added flow calculations

--- control.c
+++ control.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 #include <signal.h>
 #include <setjmp.h>
 #include <unistd.h>
@@ -94,7 +95,7 @@ float flowRead (int handle, int channel)
 
 	qsort (array, sampleSize, sizeof(*array), compareInts);
 
-	ret = (float)(array[sampleSize / 2] >> 4) * 0.1500596393 + 0.2813477731;
+	ret = (float)(array[sampleSize / 2] >> 4) * 0.1501769661 + 0.8169154351;
 	if (ret > 300.0)
 		ret = 0;
 
@@ -229,6 +230,14 @@ int main (int argc, char **argv)
 	struct timeval curTimeval;
 	double curTime;
 
+	const int travel[11] = {5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	const float Cvi[11] = {0, 0, 0, 0, 0, 0.125, 0, 0, 0, 0, 0.226};
+	const float Xti[11] = {0, 0, 0, 0, 0, 0.1916535, 0, 0, 0, 0, 0.560641};
+	const float Fdi[11] = {0.0321, 0.0738, 0.1200, 0.1533, 0.1811, 0.2055, 0.2277, 0.2483, 0.2676, 0.2860, 0.30361};
+	float Cv = 0.226;
+	float Xt = 0.560641;
+	float Fd = 0.30361;
+
 	int i;
 
 	if (setjmp (buf))
@@ -247,7 +256,8 @@ int main (int argc, char **argv)
 		if (scanf ("%s", command) == 1) {
 			if (strcmp (command, "help") == 0) {
 				puts ("Accepted commands are:");
-				puts ("set [valve_position]\tmoves the stepper-valve by the specified number of steps");
+				puts ("set [steps]\tmoves the stepper-valve by the specified number of steps");
+				puts ("val [valve_travel]\tsets Cv and Xt to the appropriate values");
 				puts ("rec [file]\t\twrites sensor data to file untill enter is pressed");
 				puts ("exit\t\t\tsafely shuts down the program (as does cntrl-c)");
 			} else if (strcmp (command, "set") == 0) {
@@ -257,10 +267,36 @@ int main (int argc, char **argv)
 				} else {
 					puts ("Bad input, enter \"help\" for list of accepted commands");
 				}
+			} else if (strcmp (command, "val") == 0) {
+				if (scanf ("%d", &setPos) == 1) {
+					for (i = 0; i < 11; i++) {
+						if (setPos == travel[i]) {
+							Cv = Cvi[i];
+							Xt = Xti[i];
+							Fd = Fdi[i];
+							printf ("Set to %d percent open\n", setPos);
+							printf ("Cv = %f, Xt = %f, Fd = %f\n", Cv, Xt, Fd);
+							break;
+						} else if (i == 10) {
+							puts ("No data for this valve position, accepted values are:");
+							for (i = 0; i < 10; i++)
+								printf ("%d, ", travel[i]);
+							printf ("%d\n", travel[10]);
+						}
+					}
+				} else {
+					puts ("Bad input, enter \"help\" for list of accepted commands");
+				}
 			} else if (strcmp (command, "rec") == 0) {
 				if (scanf ("%s", command) == 1) {
 					recFile = fopen (command, "w");
+					fprintf (recFile, "time, temp, p1, p2, flow\n");
 					do {
+						float x;
+						float C;
+						float Rev;
+						float Q;
+
 						pressure1 = pressureRead (adcHandle, 0) * 0.9830463707 - 0.5100681364;
 						pressure2 = pressureRead (adcHandle, 1) * 0.9799264548 - 0.4912727444;
 						flow = flowRead (adcHandle, 2);
@@ -269,8 +305,25 @@ int main (int argc, char **argv)
 						gettimeofday (&curTimeval, NULL);
 						curTime = (double)curTimeval.tv_sec + (double)curTimeval.tv_usec / 1e6;
 
+						x = (pressure1 - pressure2) / pressure1;
+						C = flow * 0.06 / (22.5 * pressure1 * 6.89475729) * sqrt (28.97 * (temp + 273.15) / x);
+						Rev = 7.6e-2 * Fd * flow * 0.06 / (1.461e-5 * sqrt (Cv * 0.9)) * pow (0.81 * pow (Cv, 2) / 0.277344 + 1, 0.25);
+
+						if (Rev < 10000.0) {
+							//Temp Q calculation, needs to include Fr
+							Q = 16.6666667 * 22.5 * Cv * (pressure1 * 6.89475729) * (1 - x / (3 * Xt)) * sqrt (x / (28.97 * 296.15));
+							printf ("Laminar,   ");
+						} else if (x < Xt) {
+							Q = 16.6666667 * 22.5 * Cv * (pressure1 * 6.89475729) * (1 - x / (3 * Xt)) * sqrt (x / (28.97 * 296.15));
+							printf ("Turbulent, ");
+						} else {
+							Q = 16.6666667 * 0.6666667 * 22.5 * Cv * (pressure1 * 6.89475729) * sqrt (Xt / (28.97 * 296.15));
+							printf ("Choked,    ");
+						}
+
+						printf ("%f, %f, %f, %f, %f, %f\n", pressure1, pressure2, x, C, Rev, Q);
+
 						fprintf (recFile, "%lf, %f, %f, %f, %f\n", curTime, temp, pressure1, pressure2, flow);
-						printf ("%lf, %f, %f, %f, %f\n", curTime, temp, pressure1, pressure2, flow);
 
 						fflush (stdout);
 						FD_ZERO (&s);
